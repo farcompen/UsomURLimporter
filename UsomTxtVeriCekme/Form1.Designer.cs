385f0544fc25ae5189fcebe1e06b1d87103182a0
farcompen <farcompen71@gmail.com>
2016-11-17 15:03:37 +0300

    UsomVericekme

---
+++ UsomTxtVeriCekme/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace UsomTxtVeriCekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(5, 121);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(414, 590);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.richTextBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 4);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(410, 585);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox2);
+            this.panel2.Location = new System.Drawing.Point(524, 124);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(626, 590);
+            this.panel2.TabIndex = 1;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.richTextBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox2.Location = new System.Drawing.Point(20, 4);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(602, 581);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(426, 306);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 78);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Script Oluştur >>";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(14, 73);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "USOM\'dan Veri Al ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(177, 74);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Görüntüle ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(1036, 721);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 28);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Script Kaydet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 884);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "FortiNet USOM URL importer  [T.C. Sağlık Bakanlığı ]  V 1.0 ";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ UsomTxtVeriCekme/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+using System.Net;
+
+
+namespace UsomTxtVeriCekme
+{
+    public partial class Form1 : Form
+    {
+
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+            indirme();
+            
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            script_olustur();
+            button4.Visible = true;
+        }
+
+        public void indirme()
+
+        {
+            try
+            {
+                string indirme_yolu = "https://www.usom.gov.tr/url-list.txt";
+                string pc_yolu = "C:\\";
+                string dosya_adi = "usom_list.txt";
+                WebClient webClient = new WebClient();
+
+                webClient.DownloadFileAsync(new Uri(indirme_yolu), pc_yolu + dosya_adi);
+                MessageBox.Show("Veri alındı ");
+            }
+
+
+            catch (Exception hata)
+            {
+                throw hata;
+            }
+        }
+        public void script_olustur()
+
+        {
+            try
+            {
+                richTextBox2.Text = " config firewall address \n               edit ";
+                string dosya_yolu = "C:\\usom_list.txt";
+                FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+                StreamReader SR = new StreamReader(fs);
+                string yazi = SR.ReadLine();
+
+
+                while (yazi != null)
+                {
+                    richTextBox2.Text = richTextBox2.Text + "''" + yazi + "''" + "\n    " + "                          set type fqdn   \n                              set fqdn "+ "''" + yazi+"''" + " \n               next      \n               edit ";
+                    yazi = SR.ReadLine();
+                }
+                fs.Close();
+                SR.Close();
+                richTextBox2.Text = richTextBox2.Text + "\n               end \n                 \n ";
+            }
+            catch (Exception eror)
+            {
+                throw eror;
+            }
+            
+        }
+        public void txt_dosya_getir()
+        {
+
+            try
+            {
+                richTextBox1.Text = " ";
+                string dosya_yolu = "C:\\usom_list.txt";
+                FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+                StreamReader SR = new StreamReader(fs);
+                string yazi = SR.ReadLine();
+
+
+                while (yazi != null)
+                {
+                    richTextBox1.Text = richTextBox1.Text +  yazi  + "\n";
+                    yazi = SR.ReadLine();
+                }
+                fs.Close();
+                SR.Close();
+            }
+            catch (Exception eror)
+            {
+                throw eror;
+            }
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txt_dosya_getir();
+            button1.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            kaydet();
+           
+            MessageBox.Show("Başarılı şekilde kaydedildi  ");
+
+        }
+
+        public void kaydet()
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Metin Dosyası|*.txt";
+            save.OverwritePrompt = true;
+            save.CreatePrompt = true;
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter yazici = new StreamWriter(save.FileName);
+                yazici.WriteLine(richTextBox2.Text);
+                yazici.Flush();
+               
+                yazici.Close();
+              
+
+            }
+
+        }
+
+    }
+}
